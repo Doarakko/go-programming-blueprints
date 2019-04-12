c699a097e19d4b9b1b125dd7ed360e6397e73b17
Doarakko <2wua4nlyi@gmail.com>
2019-04-13 01:50:00 +0900

    fix p16

--- chat/main.go
+++ chat/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
-	"text/template"
 )
 
 type templateHandler struct {
@@ -18,7 +18,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
